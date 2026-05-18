e6b51c45d5ebc13b4aa408718e32ce3b7c28cab3
jatinstack03 <jatinnanjan19@gmail.com>
2026-05-18 18:54:26 +0530

    update input code

--- 10_maths_raleted_functions.cpp
+++ 10_maths_raleted_functions.cpp
@@ -8,21 +8,21 @@ int main ()
     double z;
     
 
-//    z = std::max(x,y);  // max value function
+   z = std::max(x,y);  // max value function
 
-//    z = std::min(x,y);  // minimam value function 
+   z = std::min(x,y);  // minimam value function 
 
-//    z = pow(2, 3);   //  power value function 
+   z = pow(2, 3);   //  power value function 
 
-//    z = sqrt(9); // square root function 
+   z = sqrt(9); // square root function 
 
-//    z = abs(-3);  // absolute value functin that give the positive version of a nagitive number
+   z = abs(-3);  // absolute value functin that give the positive version of a nagitive number
 
-//    z = round(x); // give the rounded value like x = 3.14 is rounded value is a 3.
+   z = round(x); // give the rounded value like x = 3.14 is rounded value is a 3.
 
-//    z =ceil(x);  // if you need to round up thare's a cealling function  
+   z =ceil(x);  // if you need to round up thare's a cealling function  
 
-//    z = floor (x); // rounded down 
+   z = floor (x); // rounded down 
 
 
   std::cout << z; 

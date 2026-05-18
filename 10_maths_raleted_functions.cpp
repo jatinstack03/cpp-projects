48beb5849daf662aad0e5b287100ab3280eb3f27
jatinstack03 <jatinnanjan19@gmail.com>
2026-05-18 18:51:28 +0530

    update file

--- 10_maths_raleted_functions.cpp
+++ 10_maths_raleted_functions.cpp
@@ -1,6 +1,31 @@
 #include<iostream>
+#include <cmath>
 
 int main () 
 {
-    double x = 3;
+    double x =3.99;
+    double y =4;
+    double z;
+    
+
+//    z = std::max(x,y);  // max value function
+
+//    z = std::min(x,y);  // minimam value function 
+
+//    z = pow(2, 3);   //  power value function 
+
+//    z = sqrt(9); // square root function 
+
+//    z = abs(-3);  // absolute value functin that give the positive version of a nagitive number
+
+//    z = round(x); // give the rounded value like x = 3.14 is rounded value is a 3.
+
+//    z =ceil(x);  // if you need to round up thare's a cealling function  
+
+//    z = floor (x); // rounded down 
+
+
+  std::cout << z; 
+
+    return 0;
 }

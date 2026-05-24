cbbab01ff145e842dca2f6fa53681e413393e3f3
jatinstack03 <jatinnanjan19@gmail.com>
2026-05-24 17:24:44 +0530

    Create 16_logical_oprators.cpp

---
+++ 16_logical_oprators.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+int main()
+{
+
+ // && =  check if two conditions are true.
+ // || = check if at least one of two condtion is true.
+ // ! = reverses the logical state of its operand. 
+
+  int temp;
+  
+  bool sunny =true;
+
+  std::cout << "Enter the temperature ";
+  std::cin >> temp;
+  
+  
+  
+//  if (temp < 0 && temp < 30) {
+//     std::cout << "temperature is good";
+// }
+//  else {
+//     std::cout << "temprature is bad ";
+//  }
+
+//  return 0;
+
+
+
+// if (temp <= 0 || temp >= 30)
+// {
+//     std::cout << "temperature is bad! ";
+
+// }
+// else {
+//     std::cout << "temperature is good! ";
+
+// }
+
+
+// if(!sunny) {
+//   std::cout << "its is sunny outside ";
+// }
+// else{
+//   std::cout << "it is cloudy outside! ";
+// }
+
+ return 0; 
+}

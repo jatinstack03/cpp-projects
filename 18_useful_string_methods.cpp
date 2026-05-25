aa559510a5021104cfbfa27327a8cc9c58544cd7
jatinstack03 <jatinnanjan19@gmail.com>
2026-05-25 17:52:32 +0530

    Create 18_useful_string_methods.cpp

---
+++ 18_useful_string_methods.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+
+int main() 
+{
+    std::string name;
+    
+    std::cout << "Enter your name : ";
+
+    std::getline(std::cin,name);
+
+    // if(name.length() > 12) 
+    
+    
+    // {
+    //     std::cout << "You can't be over 12 charcters ";
+
+    // }
+    // else {
+    //     std::cout << "welcome " << name;
+    // }
+
+    // if(name.empty())
+    
+    
+    //{
+
+    // std::cout << "You did't enter your name ";
+
+    // }else {
+    //     std::cout << "Hello " << name;
+
+
+    // }
+    
+
+    // name.clear();
+
+    // std::cout << "Helloe " << name;
+
+    // name.append("@gmail.com");
+    
+    // std::cout << "Your username is now" << name;
+
+    // std::cout << name.at(0);
+    
+    // name.insert(0, "@");
+    
+    // name.erase(0, 3);
+    
+    // std::cout << name.find(' ');
+
+    //std::cout << name.find(' ');
+    
+    return  0;
+
+
+
+
+}

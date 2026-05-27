65d2cdb0a87696c6b78ca1640cb77af5e1b62f12
jatinstack03 <jatinnanjan19@gmail.com>
2026-05-27 20:28:42 +0530

    Update 21_For_loops.cpp

--- 21_For_loops.cpp
+++ 21_For_loops.cpp
@@ -2,6 +2,9 @@
 int main(){
 
     for(int i = 1;i<=10;i++)
+    // for(int i=1;i<=10;i+=2)
+    // for(int i=1;i<=10;i--)
+    // for(int i=1;i<=10;i-=2)
     {
         std::cout << i << std::endl;
         

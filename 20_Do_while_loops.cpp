f6060cf6813c6e8cc530cd4484d45528ba1075bd
jatinstack03 <jatinnanjan19@gmail.com>
2026-05-30 19:26:43 +0530

    Update 20_Do_while_loops.cpp

--- 20_Do_while_loops.cpp
+++ 20_Do_while_loops.cpp
@@ -4,6 +4,10 @@ int main()
 {
     // do while loop = DO SOME BLOCK OF CODE FIRST,
     //    THEN REPEAT AGAIN IF CONDITION IS TRUE.
+
+    // a do-while loop is an exit-controlled loop that 
+    //guarantees its internal code block will execute 
+    //at least once before evaluating the termination condition. 
     
     int number;
 

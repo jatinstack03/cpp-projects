2184139bed7b945d9a8fafe6d69873b99a41b0a0
jatinstack03 <jatinnanjan19@gmail.com>
2026-06-10 22:48:30 +0530

    Update 21_For_loops.cpp

--- 21_For_loops.cpp
+++ 21_For_loops.cpp
@@ -2,6 +2,10 @@
 
 
 // A for loop in C++ repeats a block of code a specific number of times.
+//or loops repeat a block of code a specific number of times.
+// They are entry-controlled loops, meaning the condition is checked before executing the loop body
+
+
 int main(){
 
     for(int i = 1;i<=10;i++)

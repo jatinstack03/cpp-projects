19cba4631df8142bd5449b44cb50e0208c59cfe0
jatinstack03 <jatinnanjan19@gmail.com>
2026-06-11 20:29:15 +0530

    Create 27_user_define_funtions.cpp

---
+++ 27_user_define_funtions.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+void happyBirthday(std::string name,int age);
+
+
+int main () 
+{
+        
+        // FUNCTION ===  A BLOCK OF REUSABLE CODE.....
+        std::string name = "bro ";
+        int age = 18;
+
+       happyBirthday(name, age);
+      
+       
+    return 0;
+
+
+}
+
+void happyBirthday(std::string name,int age) // parameter (you can rename perameter)
+{
+    std::cout<< " Happy brithday to you "  << name << '\n';
+    std::cout<< " Happy brithday to you "<< name << '\n';
+    std::cout<< " Happy brithday to you " << name << '\n';
+    std::cout<< " Happy brithday to you " << name << '\n';
+    std::cout<< " you are " << age << "years old \n";
+
+
+}

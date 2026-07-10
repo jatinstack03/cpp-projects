442b8a50b778c3b44fd61f4db5bd0835be8d331b
jatinstack03 <jatinnanjan19@gmail.com>
2026-07-10 18:48:54 +0530

    Update 34_iterate_over_an_array.cpp

--- 34_iterate_over_an_array.cpp
+++ 34_iterate_over_an_array.cpp
@@ -2,17 +2,15 @@
 
 int main()
 {
-    std::string student[] = {"himesh","bless","jatin","harsh"};
-      
-        // DISPLE STUDENTS...
-    
-        for(int i=0; i < sizeof(student)/sizeof(std::string); i++)
-        {
-           std::cout << student[i] << "\n";
+   std::string students[] = {"jatin","himesh","harsh"};
 
+   for(int i=0;sizeof(students)/sizeof(std::string); i++)
+   {
 
-        }
+   std::cout << students[i] << "\n";
+   }
 
-        return 0;
+   return 0;
+
 
 }

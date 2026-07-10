f9fe205d4047feb1cee68ea297508d3009a98725
jatinstack03 <jatinnanjan19@gmail.com>
2026-07-10 17:39:15 +0530

    Update 33_sizeof()_operator.cpp

--- 33_sizeof()_operator.cpp
+++ 33_sizeof()_operator.cpp
@@ -8,12 +8,14 @@ int main()
     std::string name = "bro is avsome";
     double gpa = 7.5;
     char grade = 'A';
-    bool student = "true";
+    bool student = true;
+    char grades[] = {'A','B','C','D','F'};
+    std::string student[] = {"Jatin","himesh","harsh","mohit","bless","maan"};
 
     std::cout << sizeof(gpa) << "\n";
-    std::cout << sizeof(grade) << "\n";
     std::cout << sizeof(student) << "\n";
-    
+    std::cout << sizeof(grades)/sizeof(grades[0]) << "\n";
+
 
 
 

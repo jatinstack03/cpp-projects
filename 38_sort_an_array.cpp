0e4668b55fd8de1bf3f438dacce6de7d92133c68
jatinstack03 <jatinnanjan19@gmail.com>
2026-07-18 17:38:35 +0530

    Create 38_sort_an_array.cpp

---
+++ 38_sort_an_array.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+void sort(int array[], int size);
+
+int main() {
+
+    int array[] = {3,2,1,5,4,7,6};
+    int size = sizeof(array)/ sizeof(array[0]);
+
+    sort(array,size);
+
+    for(int element : array)
+    {
+        std::cout << element << '\n';
+
+    }
+    return 0;
+}
+
+void sort(int array[], int size)
+{
+    int temp;
+
+    for(int i = 0; i < size -1; i++){
+        for(int j = 0; j < size - i - 1; j++){
+
+            if(array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+            }
+
+
+        }
+    }
+}

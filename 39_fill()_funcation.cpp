bb72370bd4a43e225df79679b7c63b09819e7503
jatinstack03 <jatinnanjan19@gmail.com>
2026-07-28 18:45:56 +0530

    Update 39_fill()_funcation.cpp

--- 39_fill()_funcation.cpp
+++ 39_fill()_funcation.cpp
@@ -1,12 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <algorithm> // Required for std::fill
-
+#include<iostream>
+ 
+// fill() == FILLS A RANGE OF ELEMENTS WITH A
+//           SPECIFIED VALUE FILL(BEGIN, END, VALUE)
 int main() {
-    std::vector<int> vec(5); // Creates a vector of size 5
-    
-    // Fill all elements with the value 42
-    std::fill(vec.begin(), vec.end(), 42); 
-    
-    return 0;
+std::string foods[100];
+
+fill(foods, foods+100,"pizza");
+
+for(std::string food : foods)
+{
+    std::cout << food << '\n';
+
+
+}
+
+return 0;
+
 }

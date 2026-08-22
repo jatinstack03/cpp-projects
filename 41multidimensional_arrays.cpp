f1e7d322b6081191c16ead4147cf640165d7df4e
jatinstack03 <jatinnanjan19@gmail.com>
2026-08-22 09:24:22 +0530

    Update 41multidimensional_arrays.cpp

--- 41multidimensional_arrays.cpp
+++ 41multidimensional_arrays.cpp
@@ -1,21 +1,26 @@
-#include <iostream>
+#include<iostream>
 
 int main() {
-    int matrix[3][4] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12}
-    };
 
-    // Print element at row 1, column 2 (Outputs 7)
-    std::cout << matrix[1][2] << std::endl;
+   int matrix[][4] = { {1,2,3,4},
+                      {5,6,7,8},
+                      {9,10,11,12} };
 
-    // Outer loop for rows, inner loop for columns
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            std::cout << matrix[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-    return 0;
+   int rows = sizeof(matrix) / sizeof(matrix[0]);
+   
+   int columns = sizeof(matrix[0]) / sizeof(matrix[0][0]);
+
+   for(int i = 0; i < rows;i++) {
+    
+      for(int j =0; j < columns; j++)
+      {
+         std::cout << matrix[i][j] << " ";
+
+
+      }
+         std::cout << "\n";
+   }
+
+   return 0;
+
 }

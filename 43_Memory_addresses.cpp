4b8469a9ac3cd8087c60957d1ef29663b2a6664b
jatinstack03 <jatinnanjan19@gmail.com>
2026-08-24 22:09:52 +0530

    Create 43_Memory_addresses.cpp

---
+++ 43_Memory_addresses.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main() {
+
+    // MEMORY ADDRESS = A LOCATION IN MEMORY WHERE DATA IS STORED
+    // A MEMORY ADDRESS CAN ACCESSED WITH & ( address-of operator )
+
+    std::string name = "MAAN BHAI";
+    int age = 21;
+    bool student = true;
+
+
+    std::cout << &name << '\n';
+    std::cout << &age << '\n';
+    std::cout << &student << '\n';
+
+    
+
+    return 0;
+}

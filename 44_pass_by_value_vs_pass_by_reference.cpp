ea3e47cfd7ffe7dbca23a39befe312e99b800e29
jatinstack03 <jatinnanjan19@gmail.com>
2026-08-27 20:18:05 +0530

    Create 44_pass_by_value_vs_pass_by_reference.cpp

---
+++ 44_pass_by_value_vs_pass_by_reference.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+void swap(std::string &x,std::string &y);
+
+
+int main() 
+{
+    std::string x = "jatin";
+    std::string y = "renuka";
+     
+
+
+    swap(x,y);
+
+    std::cout << "x: " << x << '\n';
+    std::cout << "y: " << y << '\n';
+
+    return 0;
+
+
+
+}
+void swap(std::string &x,std::string &y) {
+    std::string temp;
+
+    temp = x;
+    x = y;
+    y = temp;
+    
+}

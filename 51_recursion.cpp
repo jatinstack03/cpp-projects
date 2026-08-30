3365ad04359ff8018a71296e32885f080a2193ca
jatinstack03 <jatinnanjan19@gmail.com>
2026-08-31 02:07:05 +0530

    Create 51_recursion.cpp

---
+++ 51_recursion.cpp
@@ -0,0 +1,23 @@
+#include<iostream> 
+
+int factorial(int num);
+
+int main() {
+
+    std::cout << factorial(10);
+
+    return 0;
+
+}
+
+int factorial(int num) {
+
+    if(num > 0) {
+        return num * factorial(num - 1);
+
+    }else{
+        return 1;
+
+    }
+    
+}

7b10dbf65a2c5b52b313b7ffaab3cdc06aaec209
jatinstack03 <jatinnanjan19@gmail.com>
2026-09-11 01:57:35 +0530

    commit

---
+++ stack_using_vactor.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+
+class stack{
+
+    std::vector<int> v;
+
+    public:
+
+    void push(int val) {
+        v.push_back(val);
+
+    }
+
+    void pop(){
+        v.pop_back();
+
+    }
+
+    int top() {
+        return v[v.size() - 1];
+
+    }
+
+    bool empty() {
+        return v.size() == 0;
+
+    }
+};
+
+int main() {
+    stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    while(!s.empty()){
+        std::cout << s.top() << '\n';
+        s.pop();
+    
+    }
+
+    std::cout << '\n';
+
+    return 0;
+}
+
